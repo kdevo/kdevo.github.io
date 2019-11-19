ed7697ccd40d00c472b18c48b576ee6bc1bad6df
kdevo <kdevo@users.noreply.github.com>
2019-11-19 21:59:53 +0100

    :bug: Attempt to fix navbar

--- js/bundle.min.6348125470540daddc81eb09d9c6c2f5d3ad6b63826da04a8caead9766429603.js
+++ js/bundle.min.6348125470540daddc81eb09d9c6c2f5d3ad6b63826da04a8caead9766429603.js
@@ -36,9 +36,11 @@ window.onclick=function(e){if(e.target===modal){closeModal()}}
 document.onkeydown=function(e){if(e.key==='Escape'){closeModal()}}})();(function(){var $=document.querySelector.bind(document),$$=document.querySelectorAll.bind(document),menuActive=false
 window.onscroll=function(){var scrollPosition=window.pageYOffset||document.documentElement.scrollTop,windowHeight=window.innerHeight||document.documentElement.clientHeight||document.body.clientHeight,navHeight=$('nav').clientHeight
 if(scrollPosition>windowHeight-navHeight){$('nav').classList.add('nav-fixed')
+$('nav').removeAttribute("hidden")
 $$('nav > .logo, nav > .nav-toggle').forEach(function(el){el.style.visibility='visible'
 el.classList.add('show')
 el.classList.remove('hide')})}else{$('nav').classList.remove('nav-fixed')
+$('nav').setAttribute("hidden",true)
 $$('nav > .logo, nav > .nav-toggle').forEach(function(el){el.style.visibility='hidden'
 el.classList.add('hide')
 el.classList.remove('show')})}}
@@ -50,7 +52,13 @@ $$('.nav-full, main').forEach(function(el){el.classList.toggle('active')})})
 $$('.nav-full a').forEach(function(links){links.addEventListener('click',function(){toggle()
 $$('.nav-full, main').forEach(function(el){el.classList.toggle('active')})})})
 $('.logo').addEventListener('click',function(){if($('.nav-full').classList.contains('active')){$$('.nav-full, main').forEach(function(el){el.classList.toggle('active')})}})
-$('body').addEventListener('click',function(){if($('.nav-full').classList.contains('active')){$('html').style.overflowY='hidden'}else{$('html').style.overflowY='scroll'}})})();function setVisibility(e,visible){e.classList.add(visible?'show':'hide')
+$('body').addEventListener('click',function(){if($('.nav-full').classList.contains('active')){$('html').style.overflowY='hidden'}else{$('html').style.overflowY='scroll'}})
+function fullMobileViewport(){var element=this,viewportHeight=window.innerHeight,heightChangeTolerance=100
+$(window).resize(function(){if(Math.abs(viewportHeight-window.innerHeight)>heightChangeTolerance){viewportHeight=window.innerHeight
+update()}})
+function update(){element.style.height=(viewportHeight+'px')}
+update()}
+$$('header').forEach(function(){fullMobileViewport})})();function setVisibility(e,visible){e.classList.add(visible?'show':'hide')
 e.classList.remove(visible?'hide':'show')
 if(visible){e.style.visibility="visible"
 e.removeAttribute("hidden")}else{e.style.visibility="hidden"
