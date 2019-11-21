dccd1430f79ac6af6b446b1c43eaa476ab55874b
kdevo <kdevo@users.noreply.github.com>
2019-11-21 23:32:29 +0100

    :lipstick: Improve UI and user experience

--- js/bundle.min.9d383527d8f1df5f1f09f9261ffff3343d3dcdeb601fb31f9d61e32b0f0c84fc.js
+++ js/bundle.min.9d383527d8f1df5f1f09f9261ffff3343d3dcdeb601fb31f9d61e32b0f0c84fc.js
@@ -1,4 +1,18 @@
-'use strict';class Termynal{constructor(container='#termynal',options={}){this.container=(typeof container==='string')?document.querySelector(container):container;this.pfx=`data-${options.prefix||'ty'}`;this.startDelay=options.startDelay||600;this.typeDelay=options.typeDelay||90;this.lineDelay=options.lineDelay||1500;this.progressLength=options.progressLength||40;this.progressChar=options.progressChar||'█';this.progressPercent=options.progressPercent||100;this.showPercent=options.showPercent=='true';this.onExitCommand=options.onExitCommand;this.cursor=options.cursor||'▋';if(!options.noInit)this.init()}
+var $=document.querySelector.bind(document),$$=document.querySelectorAll.bind(document)
+function setVisibility(e,visible){e.classList.add(visible?'show':'hide')
+e.classList.remove(visible?'hide':'show')
+if(visible){e.style.visibility="visible"
+e.removeAttribute("hidden")}else{e.style.visibility="hidden"
+e.setAttribute("hidden",true)}}
+function onLogoClick(){}
+document.addEventListener('DOMContentLoaded',function(){const ITEMS=[...$$(".nav-item")]
+const SECTIONS=[...$$("main > section")].reverse()
+const THRESHOLD=340
+var oldIdx=-1
+console.log(ITEMS);console.log(SECTIONS);window.addEventListener("scroll",()=>{const idx=SECTIONS.length-SECTIONS.findIndex((sec)=>window.scrollY>sec.offsetTop-THRESHOLD)-1
+if(idx!=oldIdx){ITEMS.forEach((itm)=>itm.classList.remove("nav-item-active"))
+ITEMS[idx].classList.add("nav-item-active")
+oldIdx=idx}});},false);;'use strict';class Termynal{constructor(container='#termynal',options={}){this.container=(typeof container==='string')?document.querySelector(container):container;this.pfx=`data-${options.prefix||'ty'}`;this.startDelay=options.startDelay||600;this.typeDelay=options.typeDelay||90;this.lineDelay=options.lineDelay||1500;this.progressLength=options.progressLength||40;this.progressChar=options.progressChar||'█';this.progressPercent=options.progressPercent||100;this.showPercent=options.showPercent=='true';this.onExitCommand=options.onExitCommand;this.cursor=options.cursor||'▋';if(!options.noInit)this.init()}
 init(){this.lines=[...this.container.querySelectorAll(`[${this.pfx}]`)].concat(this.lineData);const containerStyle=getComputedStyle(this.container);this.container.style.width=containerStyle.width!=='0px'?containerStyle.width:undefined;this.container.style.minHeight=containerStyle.height!=='0px'?containerStyle.height:undefined;this.container.setAttribute('data-termynal','');this.container.innerHTML='';this.start();}
 async start(){await this._wait(this.startDelay);for(let line of this.lines){if(typeof line==='undefined'){break}
 const type=line.getAttribute(this.pfx);const delay=line.getAttribute(`${this.pfx}-delay`)||this.lineDelay;if(type=='input'){line.setAttribute(`${this.pfx}-cursor`,this.cursor);await this.type(line);await this._wait(delay);if(line.textContent==='exit'&&typeof this.onExitCommand!=='undefined'){this.onExitCommand()}}
@@ -9,7 +23,7 @@ async type(line){const chars=[...line.textContent];const delay=line.getAttribute
 async progress(line){const progressLength=line.getAttribute(`${this.pfx}-progressLength`)||this.progressLength;const progressChar=line.getAttribute(`${this.pfx}-progressChar`)||this.progressChar;const chars=progressChar.repeat(progressLength);const progressPercent=line.getAttribute(`${this.pfx}-progressPercent`)||this.progressPercent;line.textContent='';this.container.appendChild(line);for(let i=1;i<chars.length+1;i++){await this._wait(this.typeDelay);const percent=Math.round(i/chars.length*100);line.textContent=`${chars.slice(0,i)}`;if(this.showPercent){line.textContent+=` ${percent}%`}
 if(percent>progressPercent){break;}}}
 _wait(time){return new Promise(resolve=>setTimeout(resolve,time));}};(function(){var ANIMATION_TIME=500
-var $=document.querySelector.bind(document),$$=document.querySelectorAll.bind(document),modal,modalBoxes=$$('.modal-box'),openLinks=$$('.gallery-modal-link'),closeLinks=$$('.close')
+var modal,modalBoxes=$$('.modal-box'),openLinks=$$('.gallery-modal-link'),closeLinks=$$('.close')
 function openModal(){modalBoxes.forEach(function(box){box.classList.add('scale-in-center')
 box.classList.remove('scale-out-center')})}
 function closeModal(){modalBoxes.forEach(function(box){box.classList.remove('scale-in-center')
@@ -19,7 +33,7 @@ function open(modalElement){modal=modalElement
 modal.classList.add('active')
 openModal()
 var term=$(`#term-${modal.getAttribute('id')}`)
-if(term!==null&&typeof terms[modal.getAttribute('id')]==='undefined'){var termOptions=JSON.parse('{"lineDelay":250,"progressChar":"★","progressLength":14,"showPercent":"true","startDelay":800,"typeDelay":30}')
+if(term!==null&&typeof terms[modal.getAttribute('id')]==='undefined'){var termOptions=JSON.parse('{"lineDelay":250,"progressChar":"★","progressLength":16,"showPercent":"true","startDelay":800,"typeDelay":30}')
 termOptions.onExitCommand=function(){term.classList.add('scale-out-center')
 setTimeout(function(){$(`#content-${modal.getAttribute('id')}`).removeAttribute("hidden")
 term.setAttribute("hidden",true)},ANIMATION_TIME+10)}
@@ -28,12 +42,14 @@ var terms={};openLinks.forEach(function(link){link.onclick=function(e){e.prevent
 open($(e.target.getAttribute('href')))}
 const loc=window.location.href
 const seg=loc.substring(loc.lastIndexOf('/')+1)
-if(link.getAttribute('href')==seg){$("#work").scrollIntoView()
+if(link.getAttribute('href')==seg){console.log($("#work"))
+console.log("SCROLL")
+$("#work").scrollIntoView()
 open($(link.getAttribute('href')))}})
 closeLinks.forEach(function(link){link.onclick=function(e){e.preventDefault()
 closeModal()}})
 window.onclick=function(e){if(e.target===modal){closeModal()}}
-document.onkeydown=function(e){if(e.key==='Escape'){closeModal()}}})();(function(){var $=document.querySelector.bind(document),$$=document.querySelectorAll.bind(document),menuActive=false
+document.onkeydown=function(e){if(e.key==='Escape'){closeModal()}}})();(function(){menuActive=false
 window.onscroll=function(){var scrollPosition=window.pageYOffset||document.documentElement.scrollTop,windowHeight=window.innerHeight||document.documentElement.clientHeight||document.body.clientHeight,navHeight=$('nav').clientHeight
 if(scrollPosition>windowHeight-navHeight){$('nav').classList.add('nav-fixed')
 $('nav').classList.add('nav-shadow')
@@ -58,20 +74,15 @@ $(window).resize(function(){if(Math.abs(viewportHeight-window.innerHeight)>heigh
 update()}})
 function update(){element.style.height=(viewportHeight+'px')}
 update()}
-$$('header').forEach(function(){fullMobileViewport})})();function setVisibility(e,visible){e.classList.add(visible?'show':'hide')
-e.classList.remove(visible?'hide':'show')
-if(visible){e.style.visibility="visible"
-e.removeAttribute("hidden")}else{e.style.visibility="hidden"
-e.setAttribute("hidden",true)}}
-(function(){var $=document.querySelector.bind(document)
-var realmsg=$('textarea[name=message2]')
+$$('header').forEach(function(){fullMobileViewport})})();(function(){var realmsg=$('textarea[name=message2]')
 var honeypotmsg=$('textarea[name=message]')
 setVisibility(realmsg,true)
 setVisibility(honeypotmsg,false)
 honeypotmsg.removeAttribute("required")
 $('#form-contact').addEventListener('submit',function(e){e.preventDefault()
-var name=$('input[name=name]').value,email=$('input[name=email]').value,subject=$('input[name=_subject]').value,message=realmsg.value,honeypot=honeypotmsg.value
-var request=new XMLHttpRequest(),data={name:name,_replyto:email,email:email,_subject:subject,message:message,}
+var name=$('input[name=name]').value,email=$('input[name=email]').value,subject=$('input[name=_subject]').value,matter=$('select[name=matter]').value
+message=realmsg.value,honeypot=honeypotmsg.value
+var request=new XMLHttpRequest(),data={name:name,_replyto:email,email:email,_subject:subject,_matter:matter,message:message,}
 if(honeypot!==""){data._anti_spam_honeypot=honeypot}
 var sending=$('#form-sending'),submit=$('#form-submit'),thanks=$('#form-thankyou'),error=$('#form-error')
 setVisibility(submit,false)
